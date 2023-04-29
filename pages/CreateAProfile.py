2a88a020b21b31b8192df3aa4f3a632ccda6bff7
mmsarkar <medhamsarkar@gmail.com>
2023-04-29 15:46:11 -0700

    created gitignore page n worked on profile pages

---
+++ pages/CreateAProfile.py
@@ -0,0 +1,41 @@
+# where the user input will be gotten
+import streamlit as st
+
+def nextPage():
+    ws.destroy()
+    import SubmittedPage
+
+st.title("Create a Profile")
+
+#create the profile
+# take user input
+
+#get name
+st.write("Name")
+userName = st.text_input('*Input your first and last Name*')
+
+#get email
+st.write("Email")
+email = st.text_input("*Input your email*")
+
+#get phone number
+st.write("Phone number (no dashes or slashes)")
+phoneNum = st.number_input("*Phone number (ex. 9999999999)*")
+
+#get location / city
+st.write("What city are you from?")
+location = st.selectbox("*Choose an option:*", ['Riverside', 'Other'])
+
+#get number of cs courses taken
+st.write("How many computer science courses have you taken? These can be in college, coursera, or any other course.")
+courses = st.number_input()
+
+#get number of hackathons
+st.write("How many hackathons have you been to?")
+hackNum = st.number_input()
+
+#submitted button
+isFinished = st.button('Submit')
+
+if isFinished == True:
+    nextPage()
---
+++ pages/SubmittedPage.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.title("Submitted! Now go to log in!")
