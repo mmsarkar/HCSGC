75ed8f55ca43d8cf1d33f2f9658819a92bd43abe
mjboliva <madisonjulianna.oliva@gmail.com>
2023-04-29 14:20:35 -0700

    add learningResources and calendar page

---
+++ pages/2_learningResources.py
@@ -0,0 +1,12 @@
+#RESOURCES PAGE
+
+import streamlit as st
+
+st.title("Resources")
+
+st.header("Learn a language!")
+st.subheader("C++")
+st.subheader("HTML, CSS, JavaSctipt")
+st.subheader("Python")
+
+st.subheader("Learn ")
