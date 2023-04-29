50060d0f8c932203fe00517d6f041d5e01efd4b1
mjboliva <madisonjulianna.oliva@gmail.com>
2023-04-29 16:10:25 -0700

    made new pages

--- pages/2_learningResources.py
+++ pages/2_learningResources.py
@@ -2,9 +2,14 @@
 
 import streamlit as st
 
+st.set_page_config(
+    title="Learning Resources"
+)
+
 st.title("Resources")
 
 st.header("Learn a language!")
+
 st.subheader("C++")
 st.subheader("HTML, CSS, JavaSctipt")
 st.subheader("Python")
--- pages/3_calendar.py
+++ pages/3_calendar.py
@@ -2,8 +2,12 @@
 
 import streamlit as st
 
+
+st.set_page_config(
+    title="Events"
+)
+
 st.title("Upcoming Events")
 
-col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
-
+#col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
 
