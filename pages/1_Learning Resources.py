9c0dda77d929f756e3b57123ff19f8126b8f2a47
mmsarkar <medhamsarkar@gmail.com>
2023-04-30 08:00:18 -0700

    changed stuff on learning

--- pages/1_Learning Resources.py
+++ pages/1_Learning Resources.py
@@ -1,27 +1,27 @@
 #RESOURCES PAGE
 
 import streamlit as st
-# import gspread
+import gspread
 import pandas as pd
-# from gspread_dataframe import get_as_dataframe, set_with_dataframe
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
 
 st.set_page_config(
     page_title="Learning Resources"
 )
 
 #connect to the google sheet
-# gc = gspread.oauth(
-#     credentials_filename='path/to/the/credentials.json',
-#     authorized_user_filename='path/to/the/authorized_user.json'
-# )
+gc = gspread.oauth(
+    credentials_filename='path/to/the/credentials.json',
+    authorized_user_filename='path/to/the/authorized_user.json'
+)
 
-# sheet_id = "1L8WY2GKSypZH5pKJIis7Y6LKV2zY1IJO4PvtQxmQsZA"
-# sheet_name = "Language Resources"
-# #url = f"<https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}>"
-# #df = pd.read_csv(url, dtype=str).fillna("")
+sheet_id = "1L8WY2GKSypZH5pKJIis7Y6LKV2zY1IJO4PvtQxmQsZA"
+sheet_name = "Language Resources"
+#url = f"<https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}>"
+#df = pd.read_csv(url, dtype=str).fillna("")
 
-# spreadSheet = gc.open_by_key(sheet_id)
-# learnWS = spreadSheet.get_worksheet(0)
+spreadSheet = gc.open_by_key(sheet_id)
+learnWS = spreadSheet.get_worksheet(0)
 
 # #st.write(df)
 
@@ -74,14 +74,5 @@ if swChoice == "visual studio code":
     st.subheader("Resources for visual studio code")
 if swChoice == "vim":
     st.subheader("Resources for vim")
-
-import streamlit as st
-import gspread
-import pandas as pd
-from gspread_dataframe import get_as_dataframe, set_with_dataframe
-
-st.set_page_config(
-    title="Learning Resources"
-)
 
 
