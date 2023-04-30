1476ff9e3ef5767e3b3da786dad6b58f89168ff0
Vaneesha Singh <vsingh@ucr-secure-48-10-13-241-116.wnet.ucr.edu>
2023-04-29 17:29:39 -0700

    Add intro material to home page

--- pages/home.py
+++ pages/home.py
@@ -2,21 +2,27 @@
 
 import streamlit as st
 
-
 st.set_page_config(
-    page_title="Hacker CS Girls In Code",
+    page_title="hack.her",
     page_icon="👩‍💻",
     layout="wide",
     initial_sidebar_state="expanded",
 
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
+    #menu_items={
+       # 'Learning Resources': 'https://www.extremelycoolapp.com/help',
+      #  'Report a bug': "https://www.extremelycoolapp.com/bug",
+    #    'About': "# This is a header. This is an *extremely* cool app!"
+  #  }
 )
 
-#st.title("Home")
-#st.subheader("Hacker CS Girls in Code")
+st.balloons()
 
-#st.write("hot cs girlies cult")
+st.title("hack.her\n")
+st.subheader("👋 Hi there!")
+st.subheader("💻 Welcome to the official hacking hub for girls who like to code!\n")
+
+st.write("A safe space for girls interested in computer science to build their computer science skillset, ")
+st.write("find resources to learn more about different coding languages, gain information about hackathons, ")
+st.write("and most importantly, connect with other girls with the same passions.\n")
+
+#st.image(https://today.ttu.edu/posts/2021/09/Images/Woman-Computer-2-lg.jpg)
