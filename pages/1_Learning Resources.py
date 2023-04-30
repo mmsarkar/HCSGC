e96b908925069680be04dc68a4a7d84f222e7069
mmsarkar <medhamsarkar@gmail.com>
2023-04-30 07:58:42 -0700

    deleted gspread stuff

--- pages/1_Learning Resources.py
+++ pages/1_Learning Resources.py
@@ -85,27 +85,3 @@ st.set_page_config(
 )
 
 
-#connect to the google sheet
-gc = gspread.oauth(
-    credentials_filename='path/to/the/credentials.json',
-    authorized_user_filename='path/to/the/authorized_user.json'
-)
-
-sheet_id = "1L8WY2GKSypZH5pKJIis7Y6LKV2zY1IJO4PvtQxmQsZA"
-sheet_name = "Language Resources"
-#url = f"<https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}>"
-#df = pd.read_csv(url, dtype=str).fillna("")
-
-spreadSheet = gc.open_by_key(sheet_id)
-learnWS = spreadSheet.get_worksheet(0)
-
-#st.write(df)
-
-st.title("Resources")
-
-st.header("Learn a language!")
-languageChoice = st.selectbox(
-       "Choose a language:", 
-       ("C++", "C", "Python", "Java", "JavaScript", "Swift", "R"),)
-if languageChoice == "C++":
-    st.subheader("Resources for C++")
